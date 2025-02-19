56dc8b80710ca26a23cf1e2487b54f2a77bf86d1
Craig Petterson <cspetterson@hey.com>
2025-02-19 20:57:21 +0000

    Migration to add comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_19_181238) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_19_183446) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "project_id", null: false
+    t.bigint "user_id", null: false
+    t.text "content", null: false
+    t.index ["project_id"], name: "index_comments_on_project_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "projects", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -32,4 +42,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_19_181238) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "comments", "projects"
+  add_foreign_key "comments", "users"
 end
