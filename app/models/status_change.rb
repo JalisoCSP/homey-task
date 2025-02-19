158589adbfb56b95a3932dbd8bebdec9b28f394e
Craig Petterson <cspetterson@hey.com>
2025-02-19 21:44:35 +0000

    Move status into status change model

--- app/models/status_change.rb
+++ app/models/status_change.rb
@@ -2,5 +2,7 @@ class StatusChange < ApplicationRecord
   belongs_to :project
   belongs_to :user
 
-  enum :status, Project::STATUSES.index_by(&:itself)
+  STATUSES = %w[draft in_progress completed on_hold cancelled].freeze
+
+  enum :status, STATUSES.index_by(&:itself)
 end
